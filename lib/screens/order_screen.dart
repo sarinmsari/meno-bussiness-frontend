f4da862273e443b3af9b8f40200c8251fba48a3a
abk <apabhishek2002@gmail.com>
2022-02-04 17:31:11 +0530

    filter by option added

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -9,22 +9,92 @@ class OrderPage extends StatelessWidget {
   final Color colorForCook = Colors.yellow;
   final String textForWaitor = "Delivered";
   final String textForCook = "Prepared";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       appBar: CustomAppBar(title: "M E N O Business"),
       drawer: DrawyerPage(),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => OrderCard(
-          isForWaiter: isForWaiter,
-          colorForWaitor: colorForWaitor,
-          colorForCook: colorForCook,
-          textForWaitor: textForWaitor,
-          textForCook: textForCook,
-        ),
+      body: Column(
+        children: [
+          FilterBy(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (ctx, index) => OrderCard(
+                isForWaiter: isForWaiter,
+                colorForWaitor: colorForWaitor,
+                colorForCook: colorForCook,
+                textForWaitor: textForWaitor,
+                textForCook: textForCook,
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class FilterBy extends StatefulWidget {
+  const FilterBy({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<FilterBy> createState() => _FilterByState();
+}
+
+class _FilterByState extends State<FilterBy> {
+  String _currentFilterLabel = "All";
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CustomTextLabel(_currentFilterLabel),
+          PopupMenuButton(
+              child: CustomTextLabel("Filter By"),
+              onSelected: (value) {
+                print(value);
+                setState(() {
+                  if (value == 0)
+                    _currentFilterLabel = "All";
+                  else
+                    _currentFilterLabel = "Table " + value.toString();
+                });
+              },
+              itemBuilder: (ctx) => <PopupMenuItem>[
+                    PopupMenuItem(
+                      child: CustomTextLabel("All"),
+                      value: 0,
+                    ),
+                    PopupMenuItem(
+                      child: CustomTextLabel("Table 1"),
+                      value: 1,
+                    ),
+                    PopupMenuItem(
+                      child: CustomTextLabel("Table 2"),
+                      value: 2,
+                    ),
+                  ])
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTextLabel extends StatelessWidget {
+  CustomTextLabel(this.text, {Key? key}) : super(key: key);
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.blue),
     );
   }
 }
