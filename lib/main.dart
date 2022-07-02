59941097d9b6eba4238018b2db793b998ffa7dad
abk <apabhishek2002@gmail.com>
2022-07-02 13:23:34 +0530

    added firebase essentials and cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:bookkikko_business/routes.dart';
 
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
