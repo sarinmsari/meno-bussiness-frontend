22c4ae11cc55693c442f822ea3c1913f25a33779
abk <apabhishek2002@gmail.com>
2022-07-04 18:35:04 +0530

    previous commit with limit in receiving data.

--- lib/screens/role_screen.dart
+++ lib/screens/role_screen.dart
@@ -24,96 +24,122 @@ class RoleScreen extends StatelessWidget {
       drawer: DrawyerPage(),
       body: SafeArea(
         child: FutureBuilder(
-            future:
-                roles.where("restaurant_id", isEqualTo: RESTAURANT_ID).get(),
+            future: roles
+                .where("restaurant_id", isEqualTo: RESTAURANT_ID)
+                .limit(1)
+                .get(),
             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (snapshot.hasError) {
                 // throw Exception("snapshot fetching from role screen has error");
-                return Text("snapshot fetching from role screen has error");
+                return Align(
+                    child:
+                        Text("snapshot fetching from role screen has error"));
               }
-              if (!snapshot.hasData) {
+              if (snapshot.hasData && !snapshot.data!.docs[0].exists) {
                 // throw Exception("snapshot has no data");
-                return Text("snapshot has no data");
+                return Align(child: Text("snapshot has no data"));
               }
 
               if (snapshot.connectionState == ConnectionState.done) {
                 var snapshotData = snapshot.data;
                 List<Widget> allRoles = <Widget>[];
-                var restaurantRoles;
+                if (snapshotData != null) {
+                  var restaurantRoles = snapshotData.docs[0];
+                  if (restaurantRoles["roles"] != null) {
+                    (restaurantRoles["roles"] as List)
+                        .forEach((roleName) => allRoles.add(
+                              OneOrderBoxSkelton(
+                                leading: Image(
+                                  image: NetworkImage(_userUrl),
+                                ),
+                                titleText: roleName.toString().toUpperCase(),
+                                subtitle: Text("Tap to view more"),
+                                trailing: Icon(Icons.arrow_forward_ios_rounded),
+                                listTileOnTap: () {
+                                  // fetchRestaurantRoles();
 
-                // this will always have one content since we considered restaurant_id as unique for all reference
-                snapshotData!.docs.forEach((element) {
-                  restaurantRoles = element;
-                });
-                if (restaurantRoles["roles"] != null)
-                  (restaurantRoles["roles"] as List)
-                      .forEach((roleName) => allRoles.add(
-                            OneOrderBoxSkelton(
-                              leading: Image(
-                                image: NetworkImage(_userUrl),
+                                  Navigator.pushNamed(
+                                    context,
+                                    '/individual_role_screen',
+                                    arguments: IndividualRoleScreenArguments(
+                                      roleName: roleName,
+                                    ),
+                                  );
+                                },
                               ),
-                              titleText: roleName.toString().toUpperCase(),
-                              subtitle: Text("Tap to view more"),
-                              trailing: Icon(Icons.arrow_forward_ios_rounded),
-                              listTileOnTap: () {
-                                // fetchRestaurantRoles();
-                                Navigator.pushNamed(
-                                  context,
-                                  '/individual_role_screen',
-                                  arguments: IndividualRoleScreenArguments(
-                                    roleName: roleName,
-                                  ),
-                                );
-                              },
-                            ),
-                          ));
+                            ));
 
-                return ListView(children: allRoles);
-              } else
-                print("restaurantRoles['roles'] is null");
+                    return ListView(children: allRoles);
+                  }
+                } else {
+                  throw Exception("snapshotData is null");
+                }
+              }
 
-              // return ListView.builder(
-              //   itemCount: 3,
-              //   itemBuilder: (context, index) => OneOrderBoxSkelton(
-              //     leading: Image(
-              //       image: NetworkImage(_userUrl),
-              //     ),
-              //     titleText: roleName,
-              //     subtitle: Text("Tap to view more"),
-              //     trailing: Icon(Icons.arrow_forward_ios_rounded),
-              //     listTileOnTap: () {
-              //       // fetchRestaurantRoles();
-              //       Navigator.pushNamed(
-              //         context,
-              //         '/individual_role_screen',
-              //         arguments: IndividualRoleScreenArguments(
-              //           roleName: roleName,
-              //         ),
-              //       );
-              //     },
-              //   ),
-              // );
-              return Text("Loading");
+              return Align(child: CircularProgressIndicator());
             }),
       ),
     );
   }
 }
 
-// Future<List<Widget>> fetchRestaurantRoles() {
-//   var db = FirebaseFirestore.instance;
-//   List roles = [];
-//   db
-//       .collection("roles")
-//       .where("restaurant_id", isEqualTo: RESTAURANT_ID)
-//       .get()
-//       .then((snapshot) {
-//     snapshot.docs.forEach((element) {
-//       return element["roles"];
-//     });
-//   }).onError((error, stackTrace) {
-//     throw Exception(
-//         "error occured while retrieving roles from database with restaurant_id");
-//   });
-//   return <Widget>[];
+// for adding dummy database 
+
+// void addRestaurant() {
+//   var db = FirebaseFirestore.instance.collection("restaurant");
+//   db.add(
+//     {
+//       "restaurant_id": "restaurantid002",
+//       "restaurant_name": "Ithatha's Kitchen",
+//       "restaurant_logo": "",
+//       "restaurant_location": "",
+//       "restaurant_rating": "4.5",
+//       "restaurant_open_time": "7:30am",
+//       "restaurant_closing_time": "9:30pm",
+//       "restaurant_category": "multi cuisine",
+//       "membership": "standard",
+//       "categories": [
+//         "category001",
+//         "category002",
+//         "category003",
+//         "category004",
+//         "category005",
+//         "category006",
+//         "category007",
+//         "category008"
+//       ],
+//       "roles": {
+//         "manager": [
+//           {
+//             "user_id": "user111",
+//             "firstname": "Linus",
+//             "gender": "male",
+//             "phonenumber1": 1122445566
+//           }
+//         ],
+//         "chef": [
+//           {
+//             "user_id": "user211",
+//             "firstname": "Tynan",
+//             "gender": "male",
+//             "phonenumber1": 2122445566
+//           },
+//           {
+//             "user_id": "user212",
+//             "firstname": "Katherine",
+//             "gender": "female",
+//             "phonenumber1": 5128449526
+//           }
+//         ],
+//         "waiter": [
+//           {
+//             "user_id": "user311",
+//             "firstname": "Mariana",
+//             "gender": "female",
+//             "phonenumber1": 58965874
+//           }
+//         ],
+//       },
+//     },
+//   );
 // }
