afe0a8d3817dcd5ad1bfdd251c2a69d8f81ae1e4
abk <apabhishek2002@gmail.com>
2022-07-05 16:19:14 +0530

    item and category can be inserted to db

--- lib/components/menu_screen_components/menu_alert_dialog_components.dart
+++ lib/components/menu_screen_components/menu_alert_dialog_components.dart
@@ -1,106 +1,165 @@
 import 'package:bookkikko_business/components/menu_screen_components/alert_box_components.dart';
+import 'package:bookkikko_business/global_components.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MenuItemAlertDialog extends StatelessWidget {
-  const MenuItemAlertDialog({
+class MenuItemAlertDialog extends StatefulWidget {
+  MenuItemAlertDialog({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<MenuItemAlertDialog> createState() => _MenuItemAlertDialogState();
+}
+
+class _MenuItemAlertDialogState extends State<MenuItemAlertDialog> {
+  TextEditingController nameController = TextEditingController();
+
+  TextEditingController priceController = TextEditingController();
+
+  String? categoryValue;
+
+  String hintText = "Choose a category";
 
   @override
   Widget build(BuildContext context) {
     return CustomDialogBoxLayout(
       title: "Add new item",
       columnChildren: [
-        const TextField(
+        TextField(
+          controller: nameController,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             hintText: 'Item name',
           ),
         ),
         SizedBox(height: 10),
-        const TextField(
+        TextField(
+          controller: priceController,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             hintText: 'Item price',
           ),
           keyboardType: TextInputType.number,
         ),
-        PopupMenuButton(
-          child: Icon(Icons.menu),
-          itemBuilder: (ctx) => <PopupMenuItem>[
-            PopupMenuItem(
-              child: Text("item1"),
-              value: 0,
-            ),
-            PopupMenuItem(
-              child: Text("item2"),
-              value: 0,
-            ),
-            PopupMenuItem(
-              child: Text("item3"),
-              value: 0,
-            ),
-            PopupMenuItem(
-              child: Text("item4"),
-              value: 0,
-            ),
-            PopupMenuItem(
-              child: Text("item5"),
-              value: 0,
-            ),
-          ],
+        SizedBox(
+          height: 10,
         ),
-        Row(
-          children: [
-            Expanded(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  textStyle: TextStyle(fontSize: 20),
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Cancel"),
-              ),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Expanded(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  textStyle: TextStyle(fontSize: 20),
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Add"),
-              ),
-            ),
-          ],
-        )
+        FutureBuilder(
+            future: FirebaseFirestore.instance
+                .collection("categories")
+                .where("restaurant_id", isEqualTo: RESTAURANT_ID)
+                .limit(1)
+                .get(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return Align(child: Text("Something went wrong"));
+              if (snapshot.hasData && !snapshot.data!.docs[0].exists)
+                return Align(
+                  child: Text("Data not found"),
+                );
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> snapshotData =
+                    snapshot.data!.docs[0].data() as Map<String, dynamic>;
+
+                List<PopupMenuItem> popupMenuList = [];
+                snapshotData["category"].forEach((item) {
+                  popupMenuList.add(
+                    PopupMenuItem(
+                      child: Text(item["category_name"]),
+                      value: item["category_name"],
+                    ),
+                  );
+                });
+
+                return PopupMenuButton(
+                    onSelected: (value) {
+                      setState(() {
+                        categoryValue = value.toString();
+                        hintText = value.toString();
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        // color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.blue),
+                      ),
+                      height: 50,
+                      width: double.infinity,
+                      child: Center(
+                        child: Text(
+                          hintText,
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                    itemBuilder: (ctx) => popupMenuList);
+              }
+              return Align(
+                child: CircularProgressIndicator(),
+              );
+            }),
+        SizedBox(
+          height: 10,
+        ),
+        AlertBoxCancelAddButton(onAddPress: () {
+          const DOC_ID = "2HWgmtuknXoncNwGJsrP"; // assume it is restaurant_id
+
+          Navigator.pop(context);
+          FirebaseFirestore.instance
+              .collection("menu")
+              .doc(DOC_ID)
+              .get()
+              .then((value) {
+            Map<String, dynamic> data = value.data() as Map<String, dynamic>;
+            List listRef = data["items"];
+            listRef.add({
+              "item_id": "item111",
+              "item_name": nameController.text,
+              "item_category": categoryValue,
+              "item_price": priceController.text,
+              "item_rating": "",
+              "item_created_date": "2019-05-28"
+            });
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("successfully added"),
+              backgroundColor: Color.fromARGB(255, 51, 217, 57),
+            ));
+
+            value.reference.update({"items": listRef});
+          }).onError((error, stackTrace) {
+            FirebaseFirestore.instance
+                .collection("menu")
+                .doc(RESTAURANT_ID)
+                .set({
+              "item_id": "item111",
+              "item_name": nameController.text,
+              "item_category": categoryValue,
+              "item_price": priceController.text,
+              "item_rating": "",
+              "item_created_date": "2019-05-28"
+            });
+            // document not found
+          });
+        }, onCancelPress: () {
+          Navigator.pop(context);
+        })
       ],
     );
   }
 }
 
 class MenuCategoryAlertDialog extends StatelessWidget {
-  const MenuCategoryAlertDialog({Key? key}) : super(key: key);
-
+  MenuCategoryAlertDialog({Key? key}) : super(key: key);
+  TextEditingController categoryController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return CustomDialogBoxLayout(
       title: "Add new category",
       columnChildren: [
-        const TextField(
+        TextField(
+          controller: categoryController,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             hintText: 'Categoroy name',
@@ -111,6 +170,33 @@ class MenuCategoryAlertDialog extends StatelessWidget {
         ),
         AlertBoxCancelAddButton(onAddPress: () {
           Navigator.pop(context);
+          var db = FirebaseFirestore.instance;
+          CollectionReference collection = db.collection("categories");
+          collection
+              .where("restaurant_id", isEqualTo: RESTAURANT_ID)
+              .limit(1)
+              .get()
+              .then((category) {
+            List copyList = category.docs[0]["category"];
+            copyList.add({
+              "category_id": "cat111",
+              "category_name": categoryController.text
+            });
+            category.docs[0].reference.update({"category": copyList}).then(
+                (value) => print("successfully updated list"));
+          }).onError((error, stackTrace) {
+            // if no data found with existing restaurant id ,
+            // create new data set
+            collection.add({
+              "restaurant_id": "123", // change it
+              "category": [
+                {
+                  "category_id": "cat121", // change it
+                  "category_name": "cat_name" // change it
+                }
+              ]
+            }).then((value) => print("new data added"));
+          });
         }, onCancelPress: () {
           Navigator.pop(context);
         })
